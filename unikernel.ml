ed672c036a80189de3ec60e73dac04c310dc0eb8
Hannes Mehnert <hannes@mehnert.org>
2018-06-26 23:46:08 +0200

    updates

--- unikernel.ml
+++ unikernel.ml
@@ -5,15 +5,15 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) = st
   module D = Dns_mirage.Make(R)(P)(M)(T)(S)
 
   let data =
-    let zone = Dns_name.of_string_exn "nqsb.io" in
-    let n = Dns_name.prepend_exn zone
+    let zone = Domain_name.of_string_exn "nqsb.io" in
+    let n = Domain_name.prepend_exn zone
     and ip = Ipaddr.V4.of_string_exn
-    and ss = Dns_name.DomSet.of_list
+    and ss = Domain_name.Set.of_list
     in
     let ns = n "ns"
     and ttl = 2560l
     and ns' = n "sn"
-    and mx = Dns_name.of_string_exn "mail.mehnert.org"
+    and mx = Domain_name.of_string_exn "mail.mehnert.org"
     in
     let soa = Dns_packet.({ nameserver = ns ;
                             hostmaster = n "hostmaster" ;
@@ -22,20 +22,20 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) = st
     in
     let open Dns_trie in
     let open Dns_map in
-    let t = insert zone (V (K.Soa, (ttl, soa))) Dns_trie.empty in
-    let t = insert zone (V (K.Ns, (ttl, ss [ ns ; ns' ]))) t in
-    let t = insert ns (V (K.A, (ttl, [ ip "198.167.222.200" ]))) t in
-    let t = insert ns' (V (K.A, (ttl, [ ip "194.150.168.146" ]))) t in
-    let t = insert zone (V (K.A, (ttl, [ ip "198.167.222.201" ]))) t in
-    let t = insert zone (V (K.Mx, (ttl, [ (10, mx) ]))) t in
-    let t = insert (n "usenix15") (V (K.A, (ttl, [ ip "198.167.222.201" ]))) t in
-    let t = insert (n "tron") (V (K.A, (ttl, [ ip "198.167.222.201" ]))) t in
-    let t = insert (n "hannes") (V (K.A, (ttl, [ ip "198.167.222.205" ]))) t in
-    let t = insert (n "shell") (V (K.A, (ttl, [ ip "198.167.222.207" ]))) t in
-    let t = insert (n "kinda") (V (K.A, (ttl, [ ip "198.167.222.209" ]))) t in
-    let t = insert (n "tls") (V (K.A, (ttl, [ ip "198.167.222.210" ]))) t in
-    let t = insert (n "netsem") (V (K.A, (ttl, [ ip "198.167.222.213" ]))) t in
-    let t = insert (n "contao") (V (K.A, (ttl, [ ip "198.167.222.212" ]))) t in
+    let t = insert zone Soa (ttl, soa) Dns_trie.empty in
+    let t = insert zone Ns (ttl, ss [ ns ; ns' ]) t in
+    let t = insert ns A (ttl, [ ip "198.167.222.200" ]) t in
+    let t = insert ns' A (ttl, [ ip "194.150.168.146" ]) t in
+    let t = insert zone A (ttl, [ ip "198.167.222.201" ]) t in
+    let t = insert zone Mx (ttl, [ (10, mx) ]) t in
+    let t = insert (n "usenix15") A (ttl, [ ip "198.167.222.201" ]) t in
+    let t = insert (n "tron") A (ttl, [ ip "198.167.222.201" ]) t in
+    let t = insert (n "hannes") A (ttl, [ ip "198.167.222.205" ]) t in
+    let t = insert (n "shell") A (ttl, [ ip "198.167.222.207" ]) t in
+    let t = insert (n "kinda") A (ttl, [ ip "198.167.222.209" ]) t in
+    let t = insert (n "tls") A (ttl, [ ip "198.167.222.210" ]) t in
+    let t = insert (n "netsem") A (ttl, [ ip "198.167.222.213" ]) t in
+    let t = insert (n "contao") A (ttl, [ ip "198.167.222.212" ]) t in
     t
 
   let start _rng pclock mclock _ s _ _ info =
@@ -51,7 +51,7 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) = st
        Logs.err (fun m -> m "error %a during check()" Dns_trie.pp_err e) ;
        invalid_arg "check") ;
     let t =
-      UDns_server.Primary.create ~a:[UDns_server.tsig_auth]
+      UDns_server.Primary.create ~a:[UDns_server.Authentication.tsig_auth]
         ~tsig_verify:Dns_tsig.verify ~tsig_sign:Dns_tsig.sign
         ~rng:R.generate trie
     in
